879cc019bbd7e46bede04c1ad8a0a396d61571b5
Olga Vashchenkova <o.vashchenkova@gmail.com>
2021-10-16 00:58:34 +0300

    Task 1

---
+++ FileDownloader/FileDownloader/FileDownloader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+
+namespace FileDownloader
+{
+    interface IFileDownloader
+    {
+        void AddFileToDownloadingQueue(string fileId, string url, string pathToSave);
+        event Action<string> OnDownloaded;
+        event Action<string, Exception> OnFailed;
+    }
+     
+    public class FileDownloader: IFileDownloader
+    {
+        
+        public delegate void Action(string message);
+        public event Action<string> OnDownloaded;            
+        public event Action<string, Exception> OnFailed; 
+         
+        public  async void AddFileToDownloadingQueue(string fileId, string url, string pathToSave)
+        {
+          
+              Console.WriteLine("Downloading " + url);
+              Console.WriteLine("Writing to " + pathToSave);
+              var httpClient = new HttpClient();
+              byte[] fileBytes = await httpClient.GetByteArrayAsync(url);
+               
+              File.WriteAllBytes(pathToSave, fileBytes); 
+              try
+              {
+
+                  OnDownloaded?.Invoke($"Downloaded ");
+
+              }
+              catch (Exception e)
+              {
+                  Console.WriteLine(e);
+                  OnFailed?.Invoke($"Failed ", e);
+                  throw;
+              }
+              
+        }
+
+    }
+}
---
+++ FileDownloader/FileDownloaderDemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FileDownloader; 
+using System.Collections.Generic;
+using System.IO;
+namespace FileDownloaderDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logFile = File.ReadAllLines("C:/Users/Olga_Vashchenkova/RiderProjects/FileDownloader/Download/urls-list.txt");
+            var logList = new List<string>(logFile);
+            foreach(var s in logFile)
+            {
+                var filename = s.Substring(s.LastIndexOf("/")+1, s.Length-s.LastIndexOf("/")-1);
+                // Do whatever you need
+                var downloadFile = new FileDownloader.FileDownloader();
+                downloadFile.AddFileToDownloadingQueue("",s,"C:/Users/Olga_Vashchenkova/RiderProjects/FileDownloader/Download/"+filename);
+            }
+        
+        }
+    }
+}
